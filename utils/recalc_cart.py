61c0e5392a6d2537b5b07697de7a15ed859fbcb1
Yevhen <rollex60@gmail.com>
2023-08-08 22:27:08 +0100

    Updated

--- utils/recalc_cart.py
+++ utils/recalc_cart.py
@@ -1,6 +1,6 @@
 from django .db import models
 
-
+# Recalc cart
 def recalc_cart(cart):
     cart_data = cart.products.aggregate(models.Sum('final_price'), models.Count('id'))
     if cart_data.get('final_price__sum'):
